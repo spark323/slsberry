526416ae3e6848229332c1d1a99abee1c578fc8c
spark <innocentchris@gmail.com>
2023-05-16 08:06:17 +0900

    Logger 추가

--- src/lib/lambdaHandler.js
+++ src/lib/lambdaHandler.js
@@ -476,15 +476,15 @@ function iterate(apiSpec, inputObject, stack = "") {
   * @param {object} context Lambda context
   * @param {object} apiSpec API specification
   * @param {object} handler Business logic
-
+  * @param {object} Logger Logger class, 추후 Logger가 공용 모듈로 분리되면 dependancy로 두고 직접 require하도록 수정 예정
   */
-async function handleHttpRequest(event, context, apiSpec, handler) {
-
+async function handleHttpRequest(event, context, apiSpec, handler, Logger) {
+  Logger?.init?.(context);
   // input 체크
   let { inputObject, inputCheckObject } = handleTestInput(event, apiSpec);
   if (!inputCheckObject.passed) {
     console.log('Parameter not found:', inputCheckObject.result);
-
+    await Logger?.finalize?.();
     return createErrorResponseV2(422, {
       result: inputCheckObject.reason,
       parameter: inputCheckObject.stack,
@@ -494,6 +494,7 @@ async function handleHttpRequest(event, context, apiSpec, handler) {
   // test stage의 경우 echoing 기능 추가
   if (process.env.allow_mock === "true" && process.env.stage != "prod") {
     if (inputObject.mock) {
+      await Logger?.finalize?.();
       return createOKResponseV2({
         result: (typeof inputObject.mockResult === 'string') ? JSON.parse(inputObject.mockResult) : inputObject.mockResult,
         data: (typeof inputObject.mock === 'string') ? JSON.parse(inputObject.mock) : inputObject.mock,
@@ -519,7 +520,7 @@ async function handleHttpRequest(event, context, apiSpec, handler) {
           response = createPredefinedErrorResponseV2(apiSpec.errors, predefinedErrorName);
         } else {
           // 주어진 predefinedError가 apiSpec에 정의되어 있지 않은 경우
-          console.error("invalid_predefined_error", event.routeKey, event.requestContext?.apiId, inputObject, predefinedErrorName);
+          Logger?.Error("invalid_predefined_error", event.routeKey, event.requestContext?.apiId, inputObject, predefinedErrorName);
           response = createErrorResponseV2(500, {
             result: "invalid_predefined_error",
             predefinedError: predefinedErrorName,
@@ -531,22 +532,24 @@ async function handleHttpRequest(event, context, apiSpec, handler) {
     }
   } catch (error) {
     // handler 내에서 처리되지 않은 오류 발생 시 500, Internal Server Error 반환
-    console.error("Uncaught exeception in handler", event.routeKey, event.requestContext?.apiId, inputObject, error);
+    Logger?.Error("Uncaught exeception in handler", event.routeKey, event.requestContext?.apiId, inputObject, error);
     response = createErrorResponseV2(500, {
       result: "Internal Server Error",
     });
   }
-
+  await Logger?.finalize?.();
   return response;
 };
 
 
-async function handleLambdaEvent(event, context, apiSpec, handler) {
-
+async function handleLambdaEvent(event, context, apiSpec, handler, Logger) {
+  Logger?.init?.(context);
   try {
     const result = await handler(event, context);
+    await Logger?.finalize?.();
     return result;
   } catch (error) {
+    await Logger?.finalize?.();
     throw error;
   }
 }
