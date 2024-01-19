756ee50aa96ea3d142d8d96fba3545071d26c815
roy <roy@reconlabs.ai>
2024-01-19 17:18:29 +0900

    fix: bin

--- bin/cli.js
+++ bin/cli.js
@@ -9,22 +9,33 @@ const argv = yargs(hideBin(process.argv)).argv;
 
 let templateFile = argv.t ? argv.t : "serverless_template.yml";
 
-try {
-  if (argv.n) {
-    builder.uploadToNotion(
-      process.env.NOTION_SECRET ? process.env.NOTION_SECRET : argv.n,
-      process.env.STAGE,
-      process.env.VER
+const main = async () => {
+  try {
+    if (argv.n) {
+      await builder.uploadToNotion(
+        process.env.NOTION_SECRET ? process.env.NOTION_SECRET : argv.n,
+        process.env.STAGE,
+        process.env.VER
+      );
+    }
+
+    if (argv.x) {
+      await builder.generateOpenApiSpecFile(argv.x);
+    }
+
+    await builder.generateServerlessFunction(
+      `./${templateFile}`,
+      argv.stage ? argv.stage : process.env.STAGE,
+      argv.ver ? argv.ver : process.env.VER
     );
-  } else {
-    argv.x
-      ? builder.generateOpenApiSpecFile(argv.x)
-      : builder.generateServerlessFunction(
-          `./${templateFile}`,
-          argv.stage ? argv.stage : process.env.STAGE,
-          argv.ver ? argv.ver : process.env.VER
-        );
+  } catch (e) {
+    console.error(e);
   }
-} catch (e) {
-  console.error(e);
-}
+};
+
+// IIFE
+(async () => {
+  await main();
+
+  process.exit(0);
+})();
